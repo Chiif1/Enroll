184fdc911c6f2285718fc01c4cadd042fd4cbdbf
Chiif1 <nugrs@aol.com>
2021-03-26 18:19:56 -0400

    First Commit All files

---
+++ PointeProductsServe/src/main/java/PointeProductsServe/PointeProductsServer.java
@@ -0,0 +1,17 @@
+package PointeProductsServe;
+
+
+public class PointeProductsServer {
+
+	public static void main(String[] args) {
+		
+		/*RequestOptions requestOptions = (new RequestOptions.RequestOptionsBuilder())
+				  .setStripeVersion("{{API_VERSION}}")
+				  .build();
+				Map<String, Object> options = new HashMap<String, Object>();
+				options.put("customer", "{{CUSTOMER_ID}}");
+				EphemeralKey key = EphemeralKey.create(options, requestOptions);*/
+
+	}
+
+}
